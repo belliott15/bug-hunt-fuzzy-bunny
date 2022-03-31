96183777a7d9ec409251108c0d859ecd04d9ab46
Beau Elliott <belliott1516@gmail.com>
2022-03-31 11:06:01 -0700

    fixed all bugs

--- families/families.js
+++ families/families.js
@@ -7,18 +7,19 @@ import {
 
 checkAuth();
 
-const familiesEl = document.querySelector('families-container');
+const familiesEl = document.querySelector('.families-container');
 const logoutButton = document.getElementById('logout');
 
-logoutButton.addEventListener(() => {
+logoutButton.addEventListener('click', () => {
     logout();
 });
 
 // I could refactor this into some render functions. However, it becomes complicated since I need to call displayFamilies from inside each bunny's click handler.
-function displayFamilies(families) {
+async function displayFamilies(families) {
     familiesEl.textContent = '';
     
     for (let family of families) {
+        const familyEl = document.createElement('div');
         const nameEl = document.createElement('h3');
         const bunniesEl = document.createElement('div');
 
@@ -27,7 +28,7 @@ function displayFamilies(families) {
 
         nameEl.textContent = family.name;
 
-        for (let rabbit of family.fuzzy_bunnies) {
+        for (let bunny of family.fuzzy_bunnies) {
             const bunnyEl = document.createElement('div');
 
             bunnyEl.classList.add('bunny');
@@ -40,6 +41,7 @@ function displayFamilies(families) {
 
                 displayFamilies(updatedFamilies);            
             });
+            bunniesEl.append(bunnyEl);
         }
 
         familyEl.append(nameEl, bunniesEl);
@@ -48,7 +50,7 @@ function displayFamilies(families) {
 }
 
 window.addEventListener('load', async() => {
-    const families = getFamilies();
+    const families = await getFamilies();
 
     displayFamilies(families);
 });
